78f1320a0260babb2215695d52d1449b8d440d75
Nadav Babai <nadavbabai@hotmail.com>
2020-08-08 21:36:35 +0200

    [Edit] clean index.

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from "../styles/Home.module.css";
 import Article from "./article";
 import Link from "next/link";
 
-export default function Home({ data }) {
+const Index = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,18 +13,11 @@ export default function Home({ data }) {
 
       <main className={styles.main}>
         <Link href={"/article"}>click</Link>
+        <h1>hey</h1>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
   );
-}
+};
+export default Index;
