16dfef996026cba72cc4c672f3d984d2c51fa74a
Nadav Babai <nadavbabai@hotmail.com>
2020-08-09 16:55:53 +0200

    [edit] surveys added.

---
+++ elements/Iframe.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "@emotion/styled";
+
+const Iframe = ({ src = "", title = "" }) => {
+  const iframeRef = React.useRef();
+
+  return (
+    <React.Fragment>
+      <iframe
+        ref={iframeRef}
+        src={src}
+        width="100%"
+        height="100%"
+        title={title}
+        frameBorder="none"
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          display: () => (ready ? "none" : "block"),
+        }
+      }
+      />
+    </React.Fragment>
+  );
+};
+
+Iframe.propTypes = {
+  src: PropTypes.string,
+  title: PropTypes.string,
+};
+
+export default Iframe;
--- pages/surveys.js
+++ pages/surveys.js
@@ -1,27 +1,9 @@
 import React from "react";
 import styled from "@emotion/styled";
+import Iframe from "../elements/Iframe";
 
 const Surveys = () => {
-  return (
-    <FrameWrapper>
-      <iframe src="https://shakedm.co.il/surveys"></iframe>
-    </FrameWrapper>
-  );
+  return <Iframe src="https://shakedm.co.il/surveys"/>;
 };
-
-const FrameWrapper = styled.div`
-  overflow: hidden;
-  padding-top: 56.25%; /* 16:9*/
-  position: relative;
-
-  iframe {
-    border: 0;
-    height: 925px;
-    left: 0;
-    position: absolute;
-    top: 0;
-    width: 100%;
-  }
-`;
 
 export default Surveys;
