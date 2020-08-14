81052d57c3d1a4de9fc9dcaab13e372ba0552473
Nadav Babai <nadavbabai@hotmail.com>
2020-08-14 20:26:09 +0200

    feat(Services): styled services.

--- pages/services.js
+++ pages/services.js
@@ -1,22 +1,43 @@
 import React from "react";
 import axios from "axios";
 import Link from "next/link";
-import ServicePreview from "../elements/ServicePreview"
+import ServicePreview from "../elements/ServicePreview";
+import styled from "styled-components";
 
 const Services = ({ data }) => {
   return (
     <div>
-    <h1>השירותים שלנו </h1>
-
-      {data.map((service) => (
-        <Link  key={service.id} passHref href="/service/[id]" as={`/service/${service.id}`}>
-          <ServicePreview key={service.id} service={service} />
-        </Link>
-      ))}
+      <h1>השירותים שלנו </h1>
+      <StyledService>
+        {data.map((service) => (
+          <Link
+            key={service.id}
+            passHref
+            href="/service/[id]"
+            as={`/service/${service.id}`}
+          >
+            <StyledWrapper>
+              <ServicePreview key={service.id} service={service} />
+            </StyledWrapper>
+          </Link>
+        ))}
+      </StyledService>
     </div>
   );
 };
 
+const StyledService = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const StyledWrapper = styled.div`
+
+padding: 5px;
+`;
 // This gets called on every request
 export async function getServerSideProps() {
   // Fetch data from external API
