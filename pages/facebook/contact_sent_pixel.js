05e6bd02ce6d582c3f4de42ca8c145fea85279a1
Nadav Babai <nadavbabai@hotmail.com>
2020-08-24 20:04:18 +0200

    fix(Pixel) non script

--- pages/facebook/contact_sent_pixel.js
+++ pages/facebook/contact_sent_pixel.js
@@ -19,7 +19,7 @@ export default () => (
     <noscript
       dangerouslySetInnerHTML={{
         __html: `<img height="1" width="1" style="display:none"
-      src="https://www.facebook.com/tr?id=908529556229314&ev=ContactSent&noscript=1" />`,
+      src="https://www.facebook.com/tr?id=908529556229314&ev=Contact&noscript=1" />`,
       }}
     />
   </React.Fragment>
