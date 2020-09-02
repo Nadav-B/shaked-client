2251a8b3107c4e3bdc0b69212ca2c955d35a9cd1
Nadav Babai <nadavbabai@hotmail.com>
2020-09-02 13:23:57 +0200

    fix(API) console log remove.

--- shared/api.js
+++ shared/api.js
@@ -26,7 +26,6 @@ const getContacts = async () => {
 
 const postContact = async (contact) => {
   const url = `${process.env.NEXT_PUBLIC_API_INTERN_URL}/contacts/post`;
-  console.log(url);
   return await axios.post(url, contact, {
     headers: {
       Accept: "application/json",
