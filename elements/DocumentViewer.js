2abcc3a3db8528c71c0c40383063d401f09d163a
nbabai <nbabai@3pc.de>
2020-09-14 01:05:04 +0200

    fix(PDFVIEWER) fix

--- elements/DocumentViewer.js
+++ elements/DocumentViewer.js
@@ -1,22 +1,64 @@
-import styled from "styled-components";
-const DocumentViewer = ({ src }) => {
-  const url = `https://shakedm.co.il/documents/balance/${src}.pdf`;
+import React, { Component } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-  return (
-    <StyledFrame
-      src={`https://docs.google.com/viewer?url=${url}&embedded=true`}
-      width="100%"
-      height="500px"
-    />
-  );
-};
 
-const StyledFrame = styled.iframe`
-  position: absolute;
-  top: 0;
-  right: 0;
-  height: 100%;
-  z-index: 100;
+import styled from 'styled-components'
+
+export default class DocumentViewer extends Component {
+  state = { numPages: null, pageNumber: 1 };
+
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  };
+
+  goToPrevPage = () =>
+    this.setState(state => ({ pageNumber: state.pageNumber - 1 }));
+  goToNextPage = () =>
+    this.setState(state => ({ pageNumber: state.pageNumber + 1 }));
+
+  render() {
+    const { pageNumber, numPages } = this.state;
+
+    return (
+      <StyledDiv style={{ width: 600 }}>
+      <nav>
+          <button onClick={this.goToPrevPage}>Prev</button>
+          <button onClick={this.goToNextPage}>Next</button>
+        </nav>
+
+        <div style={{ width: 1200 }}>
+          <Document
+      options={{
+        cMapUrl: `//cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/cmaps/`,
+        cMapPacked: true,
+        disableFontFace:true,
+      }}
+            file="/example.pdf"
+            onLoadSuccess={this.onDocumentLoadSuccess}
+          >
+            <Page pageNumber={pageNumber} width={600} />
+          </Document>
+        </div>
+
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
+      </StyledDiv>
+    );
+  }
+}
+
+const StyledDiv = styled.div`
+
+.test {
+
+  background: red;
+  font-family: david;
+  font-size: 24px;
+}
+
+  height: ${(p) => (p.height ? p.height : p.size)}px;
+  width: ${(p) => (p.width ? p.width : p.size)}px;
 `;
 
-export default DocumentViewer;
