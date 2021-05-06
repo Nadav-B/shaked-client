16275901874e6863ae328339ef7b9f05a8a2dcd9
nbabai <nadavbabai@hotmail.com>
2021-05-06 23:25:18 +0200

    https

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,5 +1,5 @@
 const toUrl = (host, route) =>
-  `<url><loc>http://${host}${route}</loc></url>`;
+  `<url><loc>https://${host}${route}</loc></url>`;
 
 const createSitemap = (host, routes) =>
   `<?xml version="1.0" encoding="UTF-8"?>
