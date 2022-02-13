daf572e0ff8437547bc72f86c72b2947c7c2c44c
nbabai <nadavbabai@hotmail.com>
2022-02-13 14:34:37 +0100

    [EDITED] survey

--- src/pages/surveys/index.js
+++ src/pages/surveys/index.js
@@ -51,7 +51,7 @@ const StyledButton = styled.div`
   background: white;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   text-align: center;
-  margin-bottom: 30px;
+  margin: 15px;
 
 
   :hover {
