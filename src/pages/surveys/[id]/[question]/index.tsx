86e6e392deceafe8a70c23bf7cf194e4276ea0c8
nbabai <nbabai@3pc.de>
2022-04-05 15:16:38 +0200

    [EDITED] With router

--- src/pages/surveys/[id]/[question]/index.tsx
+++ src/pages/surveys/[id]/[question]/index.tsx
@@ -1,16 +1,107 @@
-import React from "react";
+import React, {useState} from "react";
 
 import styled from "@emotion/styled";
 import {useRouter} from "next/router";
+import Flex from "../../../../elements/Flex";
+import Text from "../../../../elements/Text";
+import Button from "../../../../elements/Button";
+import survey1 from "../../../../../public/surveys/1.json";
+import survey2 from "../../../../../public/surveys/2.json";
+import {Progress} from "react-sweet-progress";
 
 const QuestionView = () => {
     const router = useRouter()
     console.log(router)
-    const  questionIndex = router.query["question"];
-    const  surveyId = router.query["id"];
+    const index = Number(router.query["question"]);
 
+    const surveyId = router.query["id"];
+    const surveys = [survey1, survey2];
+    const selectedSurvey = surveys[Number(surveyId)];
+
+    const handleAnswerSubmit = (event, question, answer) => {
+        event.preventDefault();
+        results.set(question, answer);
+
+        if (index < selectedSurvey.questions.length - 1) {
+            console.log(router);
+            var path = `/surveys/${surveyId}/${index+1}`;
+            router.push(path);
+        }
+
+        if(index ==selectedSurvey.questions.length-1) {
+            var path = `/surveys/${surveyId}`;
+            router.push(path);
+        }
+
+
+    };
+
+
+
+    const backQuestion = () => {
+        //event.preventDefault();
+        if (index > 0) {
+            var path = `/surveys/${surveyId}/${index-1}`;
+            router.push(path);
+        }
+
+    };
+
+
+    const [results, setResults] = useState(new Map<String, String>());
+
+    const isChoosen = (question, answer) => {
+        if (results.get(question) === answer) return true;
+        return false;
+    };
     return (
-        <div>  {surveyId}{questionIndex}</div>
+        <Flex margin="30px" alignItems="center" flexDirection="column">
+            <Text fontSize="large">
+                שאלה {index + 1} מתוך {selectedSurvey.questions.length}
+            </Text>
+            <Progress
+                theme={{
+                    active: {
+                        symbol: "‍",
+                        color: "#0a589d",
+                    },
+                }}
+                percent={(index / selectedSurvey.questions.length) * 100}
+            />
+            <Text fontSize="large">
+                {" "}
+                {selectedSurvey.questions[index].question}
+            </Text>
+
+            <StyledAnswersWrapper>
+                {selectedSurvey.questions[index].answers.map((answer, counter) => (
+                    <Button
+                        key={counter}
+                        active={isChoosen(
+                            selectedSurvey.questions[index].question,
+                            answer
+                        )}
+                        onClick={(event) => {
+                            handleAnswerSubmit(
+                                event,
+                                selectedSurvey.questions[index].question,
+                                answer
+                            );
+                        }}
+                    >
+                        {answer}
+                    </Button>
+                ))}
+            </StyledAnswersWrapper>
+            {index > 0 && (
+                <BackButtonWrapper>
+                    <Button onClick={backQuestion} type="submit">
+                        <img src="/assets/back.svg" alt=">"/>
+                        <Text>לשאלה הקודמת</Text>
+                    </Button>
+                </BackButtonWrapper>
+            )}
+        </Flex>
     );
 
 }
