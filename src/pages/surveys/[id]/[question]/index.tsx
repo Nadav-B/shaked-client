b11b6066af760322f7785770b8f50580dcb3dd41
nbabai <nbabai@3pc.de>
2022-04-06 17:10:59 +0200

    [EDITED] Survey

--- src/pages/surveys/[id]/[question]/index.tsx
+++ src/pages/surveys/[id]/[question]/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import styled from "@emotion/styled";
 import {useRouter} from "next/router";
 import Flex from "../../../../elements/Flex";
@@ -7,116 +7,36 @@ import Button from "../../../../elements/Button";
 import surveys from "../../../../../public/surveys";
 import {Progress} from "react-sweet-progress";
 import Meta from "../../../../components/Meta";
-import TextWrapper from "../../../../elements/TextWrapper";
-import Loading from "../../../../elements/Loading";
-import {ContactInput, SurveyInput} from "../../../../graphql/__generated__/globalTypes";
-import {useMutation} from "@apollo/client";
-import mutation from "../../../../graphql/CreateContact.graphql";
-import {CreateContact} from "../../../../graphql/__generated__/CreateContact";
+import Link from "next/link";
+
 
 const QuestionView = ({id}) => {
 
-    const Status = {
-        Questions: 1,
-        CompleteContact: 2,
-        statusContact: 3,
-    };
-
-    const [currentStatus, setCurrentStatus] = useState(Status.Questions);
+    const selectedSurvey = surveys[Number(id)];
+    const selectedSurveyCache = `survey_+ ${selectedSurvey.id}`;
     const router = useRouter()
     const index = Number(router.query["question"]);
-
-    const selectedSurvey = surveys[Number(id)];
     const [results, setResults] = useState(new Map<String, String>());
+    const [selectedAnswer, setSelectedAnswer] = useState("")
     const currentQuestion = selectedSurvey.questions[index].question;
 
+    useEffect(() => {
+        if (typeof window !== "undefined" && results.size == 0) {
+            var temporal = localStorage.getItem(selectedSurveyCache);
+            setResults(new Map(JSON.parse(temporal)));
+        }
 
-    const [submitContact, {data, loading, error}] = useMutation<{ CreateContact: CreateContact },
-        { contactInput: ContactInput },
-        { surveyInput: SurveyInput }>(mutation);
-
-    console.log(results)
-    const saveSurvey = (results) => {
-        window.localStorage.setItem("results", JSON.stringify(Array.from(results.entries())));
-    }
-    const [confirmation, setConfirmation] = useState({
-        text: "",
-        style: "",
-        status: null,
-    });
-
-    const [contact, setContact] = useState({
-        fullName: "",
-        phoneNumber: "",
-        category: "שאלון",
-    });
-
-    const parseAnswersForSubmit = () => {
-        var tempArray = [];
-
-        results.forEach((key, value) => {
-            const answer = {
-                question: key,
-                answer: value,
-            };
-            tempArray.push(answer);
-        });
-
-        return tempArray;
-    };
+    }, [results]);
 
 
-    const handleChange = (event) => {
-        event.preventDefault();
-
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-
-        setContact((prevState) => ({
-            ...prevState,
-            [name]: value,
-        }));
-    };
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        setCurrentStatus(Status.statusContact);
-
-        const survey: SurveyInput = {
-            name: selectedSurvey.name,
-            answers: parseAnswersForSubmit(),
-        };
-
-        const contactForm = {
-            fullName: contact.fullName,
-            phoneNumber: contact.phoneNumber,
-            category: contact.category,
-            survey: survey,
-        };
-
-        submitContact({
-            variables: {
-                contactInput: contactForm,
-            },
-        }).then(
-            (response) => {
-                setConfirmation((prevState) => ({
-                    ...prevState,
-                    text: "פרטייך נשלחו בהצלחה, ניצור קשר בהקדם ",
-                    style: "success",
-                    status: true,
-                }));
-            },
-            (error) => {
-                setConfirmation((prevState) => ({
-                    ...prevState,
-                    text: "מצטערים אך חלה שגיאה בשליחת השאלון ניתן לפנות בפרטים המופעים בתחתית העמוד",
-                    style: "error",
-                    status: false,
-                }));
+    useEffect(() => {
+        const cachedAnswer = results.get(currentQuestion);
+        if (cachedAnswer != null) {
+            if (cachedAnswer != null) {
+                setSelectedAnswer(cachedAnswer.toLocaleString());
             }
-        );
-    };
+        }
+    });
 
 
     const seo = {
@@ -126,157 +46,78 @@ const QuestionView = ({id}) => {
     };
 
 
-    const getSurvey = () => {
-        if (typeof window !== "undefined") {
-            var temporal = window.localStorage.getItem("results");
-            setResults(new Map(JSON.parse(temporal)))
-        }
-    }
-
-    if (results.size == 0) {
-        getSurvey();
-    }
-
-
-    const handleAnswerSubmit = (event, question, answer) => {
+    const handleAnswerSubmit = (event, answer) => {
         event.preventDefault();
-        results.set(question, answer);
+        results.set(currentQuestion, answer);
+        setResults(results);
+        window.localStorage.setItem(selectedSurveyCache, JSON.stringify(Array.from(results.entries())));
 
-        saveSurvey(results);
         if (index < selectedSurvey.questions.length - 1) {
-            console.log(router);
             var path = `/surveys/${id}/${index + 1}`;
             router.push(path);
         }
 
         if (index == selectedSurvey.questions.length - 1) {
-            setCurrentStatus(Status.CompleteContact);
-        }
-
-
-    };
-
-
-    const backQuestion = () => {
-        //event.preventDefault();
-        if (index > 0) {
-            var path = `/surveys/${id}/${index - 1}`;
+            var path = `/surveys/${id}/submit`;
             router.push(path);
         }
-
     };
+
 
     return (
         <Flex alignItems={"center"} flexDirection={"column"}>
             <Meta seo={seo}/>
             <h1 className="title">{selectedSurvey.name}</h1>
-            {currentStatus == 1 && (
-                <Flex margin="30px" alignItems="center" flexDirection="column">
-                    <Text fontSize="large">
-                        שאלה {index + 1} מתוך {selectedSurvey.questions.length}
-                    </Text>
-                    <Progress
-                        theme={{
-                            active: {
-                                symbol: "‍",
-                                color: "#0a589d",
-                            },
-                        }}
-                        percent={(index / selectedSurvey.questions.length) * 100}
-                    />
-                    <Text fontSize="large">
-                        {" "}
-                        {currentQuestion}
-                    </Text>
+            <Flex margin="30px" alignItems="center" flexDirection="column">
+                <Text fontSize="large">
+                    שאלה {index + 1} מתוך {selectedSurvey.questions.length}
+                </Text>
+                <Progress
+                    theme={{
+                        active: {
+                            symbol: "‍",
+                            color: "#0a589d",
+                        },
+                    }}
+                    percent={(index / selectedSurvey.questions.length) * 100}
+                />
+                <Text fontSize="large">
+                    {" "}
+                    {currentQuestion}
+                </Text>
 
-                    <StyledAnswersWrapper>
-                        {selectedSurvey.questions[index].answers.map((answer, counter) => (
-                            <Button
-                                key={counter}
-                                active={results.get(currentQuestion) == answer}
-                                onClick={(event) => {
-                                    handleAnswerSubmit(
-                                        event,
-                                        currentQuestion,
-                                        answer
-                                    );
-                                }}
-                            >
-                                {answer}
-                            </Button>
-                        ))}
-                    </StyledAnswersWrapper>
-                    {index > 0 && (
+                <StyledAnswersWrapper>
+                    {selectedSurvey.questions[index].answers.map((answer, counter) => (
+                        <Button
+                            key={counter}
+                            active={answer == selectedAnswer}
+                            onClick={(event) => {
+                                handleAnswerSubmit(
+                                    event,
+                                    answer
+                                );
+                            }}
+                        >
+                            {answer}
+                        </Button>
+                    ))}
+                </StyledAnswersWrapper>
+                {index > 0 && (
+                    <Link
+                        passHref
+                        href={`/surveys/${selectedSurvey.id}/${index - 1}`}
+                    >
+
                         <BackButtonWrapper>
-                            <Button onClick={backQuestion} type="submit">
+                            <Button type="submit">
                                 <img src="/assets/back.svg" alt=">"/>
                                 <Text>לשאלה הקודמת</Text>
                             </Button>
                         </BackButtonWrapper>
-                    )}
-                </Flex>
-            )}
-            {currentStatus == 2 && (
-                <Flex margin="30px" alignItems="center" flexDirection="column">
-                    <Progress percent={100}/>
-                    <StatusWrapper>
-                        <Text variant="semiBold">
-                            מלאו את שמכם וטלפון ונציגנו יצרו עמכם קשר להשלמת בדיקה ללא עלות
-                        </Text>
-                    </StatusWrapper>
+                    </Link>
 
-                    <TextWrapper>
-                        <form id="submitSurveyForm" onSubmit={handleSubmit}>
-                            <Flex flexDirection="column">
-                                <label>
-                                    {contact.fullName !== "" && <Text size={"small"}>שם</Text>}
-                                    <StyledInput
-                                        name="fullName"
-                                        value={contact.fullName}
-                                        placeholder="שם מלא"
-                                        type="text"
-                                        onChange={handleChange}
-                                        required
-                                    />{" "}
-                                </label>
-
-                                <label>
-                                    {contact.phoneNumber !== "" && <>טלפון</>}
-                                    <StyledInput
-                                        name="phoneNumber"
-                                        value={contact.phoneNumber}
-                                        placeholder="טלפון"
-                                        onChange={handleChange}
-                                        type="tel"
-                                        required
-                                    />{" "}
-                                </label>
-
-                                <Button
-                                    id="submitSurvey"
-                                    disabled={confirmation.status}
-                                    type="submit"
-                                >
-                                    שלח
-                                </Button>
-                            </Flex>
-                        </form>
-                    </TextWrapper>
-                </Flex>
-            )}
-
-            {currentStatus == 3 && (
-                <StatusWrapper>
-                    {confirmation.status == null && (
-                        <div>
-                            <Loading/>
-                            <Text>שולח פרטים</Text>
-                        </div>
-                    )}
-
-                    <Text variant={confirmation.style}>{confirmation.text}</Text>
-                </StatusWrapper>
-            )}
+                )}
+            </Flex>
         </Flex>
     );
 }
@@ -296,30 +137,7 @@ const StyledAnswersWrapper = styled.div`
   margin: auto;
 `;
 
-const StatusWrapper = styled.div`
-  display: block;
-  width: 100%;
-  max-width: 400px;
-  margin: auto;
-  text-align: center;
-`;
 
-const StyledInput = styled.input`
-  width: 100%;
-  font-size:16px; 
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid ${(p) => p.theme.colors.veryLightGrey};
-  border-radius: 4px;
-  box-sizing: border-box;
-  background: ${(p) => p.theme.colors.veryLightGrey};
-  &::placeholder {
-    color: ${(p) => p.theme.colors.black};
-  }
-`;
-
-// This gets called on every request
 export async function getServerSideProps({query}) {
     const id = query.id;
     return {props: {id}};
---
+++ src/pages/surveys/[id]/submit.tsx
@@ -0,0 +1,234 @@
+import Flex from "../../../elements/Flex";
+import Text from "../../../elements/Text";
+import Button from "../../../elements/Button";
+import React, {useEffect, useState} from "react";
+import surveys from "../../../../public/surveys";
+import {ContactInput, SurveyInput} from "../../../graphql/__generated__/globalTypes";
+import styled from "@emotion/styled";
+import {useMutation} from "@apollo/client";
+import mutation from "../../../graphql/CreateContact.graphql";
+import {CreateContact} from "../../../graphql/__generated__/CreateContact";
+import {Progress} from "react-sweet-progress";
+import Loading from "../../../elements/Loading";
+import {useRouter} from "next/router";
+
+const SurveySummary = ({id}) => {
+
+    const router = useRouter();
+    const selectedSurveyCache = `survey_+ ${id}`;
+    const selectedSurvey = surveys[Number(id)];
+    const [results, setResults] = useState(new Map<String, String>());
+    const [submitContact, {data, loading, error}] = useMutation<{ CreateContact: CreateContact },
+        { contactInput: ContactInput },
+        { surveyInput: SurveyInput }>(mutation);
+
+    const [confirmation, setConfirmation] = useState({
+        text: "",
+        style: "",
+        status: null,
+    });
+
+    const [contact, setContact] = useState({
+        fullName: "",
+        phoneNumber: "",
+        category: "שאלון",
+    });
+
+    useEffect(() => {
+        if (typeof window !== "undefined" && results.size == 0) {
+            const temporal = localStorage.getItem(selectedSurveyCache);
+            setResults(new Map(JSON.parse(temporal)));
+
+
+        }
+
+    }, [results]);
+
+    const parseAnswersForSubmit = () => {
+        var tempArray = [];
+
+        results.forEach((key, value) => {
+            const answer = {
+                question: key,
+                answer: value,
+            };
+            tempArray.push(answer);
+        });
+
+        return tempArray;
+    };
+
+
+    const handleChange = (event) => {
+        event.preventDefault();
+
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        setContact((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+
+    const clearSurvey = () => {
+        localStorage.setItem(selectedSurveyCache, null);
+    }
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const survey: SurveyInput = {
+            name: selectedSurvey.name,
+            answers: parseAnswersForSubmit(),
+        };
+
+        const contactForm = {
+            fullName: contact.fullName,
+            phoneNumber: contact.phoneNumber,
+            category: contact.category,
+            survey: survey,
+        };
+
+        submitContact({
+            variables: {
+                contactInput: contactForm,
+            },
+        }).then(
+            (response) => {
+                console.log(response)
+                setConfirmation((prevState) => ({
+                    ...prevState,
+                    text: "פרטייך נשלחו בהצלחה, ניצור קשר בהקדם ",
+                    style: "success",
+                    status: true,
+                }));
+
+            },
+            (error) => {
+                setConfirmation((prevState) => ({
+                    ...prevState,
+                    text: "מצטערים אך חלה שגיאה בשליחת השאלון ניתן לפנות בפרטים המופעים בתחתית העמוד",
+                    style: "error",
+                    status: false,
+                }));
+            }
+        ).catch(reason => {
+
+            setConfirmation((prevState) => ({
+
+                ...prevState,
+                text: "מצטערים אך חלה שגיאה בשליחת השאלון ניתן לפנות בפרטים המופעים בתחתית העמוד",
+                style: "error",
+                status: false,
+            }));
+        })
+
+        clearSurvey();
+    };
+
+
+    return (
+        <Flex margin="30px" alignItems="center" flexDirection="column">
+            <h1 className="title">{selectedSurvey.name} {confirmation.status}
+            </h1>
+            {confirmation.status == null &&
+                <div>
+
+
+                    <Progress percent={100}/>
+                    <StatusWrapper>
+                        <Text variant="semiBold">
+                            מלאו את שמכם וטלפון ונציגנו יצרו עמכם קשר להשלמת בדיקה ללא עלות
+                        </Text>
+                    </StatusWrapper>
+                    <form id="submitSurveyForm" onSubmit={handleSubmit}>
+                        <Flex flexDirection="column">
+                            <label>
+                                {contact.fullName !== "" && <Text size={"small"}>שם</Text>}
+                                <StyledInput
+                                    name="fullName"
+                                    value={contact.fullName}
+                                    placeholder="שם מלא"
+                                    type="text"
+                                    onChange={handleChange}
+                                    required
+                                />{" "}
+                            </label>
+                            <label>
+                                {contact.phoneNumber !== "" && <>טלפון</>}
+                                <StyledInput
+                                    name="phoneNumber"
+                                    value={contact.phoneNumber}
+                                    placeholder="טלפון"
+                                    onChange={handleChange}
+                                    type="tel"
+                                    required
+                                />{" "}
+                            </label>
+                            <Button
+                                id="submitSurvey"
+                                disabled={confirmation.status}
+                                type="submit"
+                            >
+                                שלח
+                            </Button>
+                        </Flex>
+                    </form>
+                </div>
+            }
+
+            {confirmation.status != null && <ConfirmationComponent confirmation={confirmation}/>}
+        </Flex>
+    )
+        ;
+}
+
+
+const ConfirmationComponent = ({confirmation}) => {
+    return (
+        <StatusWrapper>
+            {confirmation.status && (
+                <div>
+                    <Loading/>
+                    <Text>שולח פרטים</Text>
+                </div>
+            )}
+            <Text variant={confirmation.style}>{confirmation.text}</Text>
+        </StatusWrapper>
+    )
+};
+
+
+const StyledInput = styled.input`
+  width: 100%;
+  font-size:16px; 
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid ${(p) => p.theme.colors.veryLightGrey};
+  border-radius: 4px;
+  box-sizing: border-box;
+  background: ${(p) => p.theme.colors.veryLightGrey};
+  &::placeholder {
+    color: ${(p) => p.theme.colors.black};
+  }
+`;
+
+export async function getServerSideProps({query}) {
+    const id = query.id;
+    return {props: {id}};
+}
+
+const StatusWrapper = styled.div`
+  display: block;
+  width: 100%;
+  max-width: 400px;
+  margin: auto;
+  text-align: center;
+`;
+
+export default SurveySummary;
