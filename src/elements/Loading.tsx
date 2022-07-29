5481b36b93bd13443dc3a3ebdf30e097986cb452
nbabai <nadavbabai@hotmail.com>
2022-07-29 09:56:07 +0200

    [EDITED] Loading tsx

---
+++ src/elements/Loading.tsx
@@ -0,0 +1,12 @@
+import { RotateCircleLoading } from "react-loadingg";
+import Flex from "./Flex";
+
+const Loading = () => {
+  return (
+    <Flex>
+      <RotateCircleLoading color="#21646b" />
+    </Flex>
+  );
+};
+
+export default Loading;
